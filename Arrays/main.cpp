d3dc36c9f5b0a349e9e213ac19eba36ab7fb71f8
r1xez <164931719+r1xez@users.noreply.github.com>
2024-10-31 21:09:38 +0200

    completed

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -1,10 +1,25 @@
-#include <iostream>
+#include <iostream> 
 
 using namespace std;
 
 int main()
+
 {
 
+    int n;
 
-	return 0;
+    cout << "Enter a number: ";
+
+    cin >> n;
+
+    int arr[3] = { n, n * 2, n * 4 };
+
+    cout << "The array is: ";
+
+    for (int i = 0; i < 3; i++)
+
+        cout << arr[i] << " ";
+
+    return 0;
+
 }
